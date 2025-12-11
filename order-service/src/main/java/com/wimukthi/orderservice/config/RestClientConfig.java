6e2349aa4b5eb1f7703d6fe3a3ecbabeb0e21044
WimukthiMadushan <bandarawgwm.21@uom.lk>
2025-12-11 20:07:03 +0530

    add cuircuit breaker

--- order-service/src/main/java/com/wimukthi/orderservice/config/RestClientConfig.java
+++ order-service/src/main/java/com/wimukthi/orderservice/config/RestClientConfig.java
@@ -1,14 +1,17 @@
 package com.wimukthi.orderservice.config;
 
 import com.wimukthi.orderservice.client.InventoryClient;
-import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,9 +24,18 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(3000);
+        return factory;
+    }
+
 }
